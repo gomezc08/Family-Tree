2374e00c608587b61380179f16f33e627062004f
Chris G <gomezc@whitman.edu>
2024-06-03 09:25:52 -0700

    rough draft of households (still incorrect btw)

--- bio/views.py
+++ bio/views.py
@@ -5,19 +5,22 @@ def get_member_info(request, PersonID):
     person = get_object_or_404(Person, Id=PersonID)
 
     # Retrieve spouse relationships
-    spouse_relationships = Spouse.objects.filter(Spouse1ID=PersonID) | Spouse.objects.filter(Spouse2ID=PersonID)
+    spouse_relationships = Spouse.objects.filter(Spouse1ID=person) | Spouse.objects.filter(Spouse2ID=person)
 
-    # Retrieve households (parents or children)
-    households = Household.objects.filter(ParentsID=PersonID) | Household.objects.filter(ChildID=PersonID)
+    # Retrieve parent relationships
+    parents = Person.objects.filter(child_relationships__ChildID=person)
+
+    # Retrieve child relationships
+    children = Person.objects.filter(parent_relationships__ParentsID=person)
 
     # Retrieve interests
-    interests = Interests.objects.filter(PersonID=PersonID)
+    interests = Interests.objects.filter(PersonID=person)
 
     context = {
         'person': person,
         'spouse_relationships': spouse_relationships,
-        'households': households,
+        'parents': parents,
+        'children': children,
         'interests': interests,
     }
     return render(request, 'bio/member_info.html', context)
-
